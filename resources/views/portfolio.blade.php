434b4855610bdf47f5a78d06e6d3ced840ea3487
Roque <roposar@hotmail.com>
2020-06-12 11:55:16 -0500

    Controladores Leccion 15

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -9,7 +9,7 @@
     <ul>
 
         @forelse ($portfolio as $portfolioItem)
-                <li> {{$portfolioItem['title']}} <small>{{ $loop->first ? 'Es el Primero' : '' }}</small>  </li>
+                <li> {{$portfolioItem['title']}} </li>
             @empty
 
                 <li>No hay proyectos para Mostrar</li>
