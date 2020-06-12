4c08ae23b0e4bb0e5e7e8b06ca659fd5e6734d8c
Roque <roposar@hotmail.com>
2020-06-12 11:18:05 -0500

    Estructuras de control con Blade - Leccion 14

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -3,6 +3,17 @@
 @section('title','Portafolio')
 
 @section('content')
+
     <h1>Portafolio</h1>
+
+    <ul>
+
+        @forelse ($portfolio as $portfolioItem)
+                <li> {{$portfolioItem['title']}} <small>{{ $loop->first ? 'Es el Primero' : '' }}</small>  </li>
+            @empty
+
+                <li>No hay proyectos para Mostrar</li>
+        @endforelse
+    </ul>
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,15 @@
 <?php
-
+$portfolio=[
+    ['title' => 'Proyecto #1' ],
+    ['title' => 'Proyecto #2' ],
+    ['title' => 'Proyecto #3' ],
+    ['title' => 'Proyecto #4' ],
+];
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-Route::view('/portfolio', 'portfolio')->name('portfolio');
+Route::view('/portfolio', 'portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
 
 
 
-
- /* Route::get('/', function () {
-    $nombre = 'Roque Pava';
-    return view('home',compact('nombre'));
- })->name('home');
- */
