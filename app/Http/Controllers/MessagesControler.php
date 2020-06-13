0ea6d8d6d37aaee1308d99d3c423260194253cdb
Roque <roposar@hotmail.com>
2020-06-13 11:02:05 -0500

    Como enviar Emails Leccion 21

--- app/Http/Controllers/MessagesControler.php
+++ app/Http/Controllers/MessagesControler.php
@@ -2,18 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
 class MessagesControler extends Controller
 {
     public function store()
     {
-        request()->validate([
+        $message= request()->validate([
             'name' => 'required',
             'email' => 'required|email',
             'subject' => 'required',
             'content' => 'required|min:3'
-        ],[
+        ], [
             'name.required' => __('I need your name')
         ]);
-        return 'Datos validados';
+
+        Mail::to('roposar@hotmail.com')->queue(new MessageReceived($message));
+
+        
+        return 'Mensaje Enviado';
     }
 }
